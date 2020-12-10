e10b9b44c14847a6455d8967887888e433d0768b
Jason Reed <Jreed40@illinois.edu>
2020-12-10 16:12:18 -0600

    init files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(<Main/>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const url = 'https://pdfmergery.herokuapp.com'
+
+function Main(props) {
+  const [files, setFiles] = useState([])
+  const [success, setSuccess] = useState("")
+  const handleFile = (event) => {
+    setFiles(event.target.files)
+  }
+  const upload = () => {
+    const data = unload()
+    console.log(files)
+    axios.post(url + '/upload', data, {}).then(res => {
+      if (res.data === "ok") {
+        setSuccess("Ready for Download!")
+      }
+    })
+  }
+  const download = () => {
+    setSuccess("")
+    axios(url + '/retrieve', {
+      method: "GET",
+      responseType: 'blob'
+    }).then(response => {
+      const file = new Blob([response.data], {
+        type: "application/pdf"
+      });
+      const fileURL = URL.createObjectURL(file);
+      window.open(fileURL)
+    })
+  }
+  const unload = () => {
+    const data = new FormData()
+    for (var i = 0; i < files.length; i++) {
+      data.append('files', files[i])
+    }
+    return data
+  }
+  return (
+    <div>
+    <input type='file' multiple onChange={handleFile}/>
+    <button onClick={upload}>Submit</button>
+    <button onClick={download}>Download</button>
+    <h1>{success}</h1>
+    </div>
+  )
+}
